779f3a704233a725510e7f5453468912fb26f912
Samuele-Chioda <samuelechioda@gmail.com>
2024-06-27 18:53:38 +0200

    sistemazione template e aggiunta condizione

--- index.php
+++ index.php
@@ -12,42 +12,52 @@
 <body>
 
     <?php
-    // recupero il contenuto del file arrayHotels
+    // Include il file che contiene l'array degli hotel
     require_once __DIR__ . '/arrayHotels.php';
-
     ?>
+
     <main>
+        <!-- Form per filtrare gli hotel con parcheggio -->
         <div class="form">
-            <form action="./index.php" method="GET"></form>
-            <label for="parking">Mostra solo hotel con parcheggio libero</label>
-            <input type="checkbox" name="parking" id="parking" <?= isset($_GET['parking']) ? 'checked' : '' ?>>
-            <button type="submit">Invia</button>
+            <form action="./index.php" method="GET">
+                <label for="parking">Mostra solo hotel con parcheggio disponibile</label>
+                <!-- Checkbox per il filtro del parcheggio -->
+                <input type="checkbox" name="parking" id="parking" <?= isset($_GET['parking']) ? 'checked' : '' ?>>
+                <button type="submit">Invia</button>
+            </form>
         </div>
+        
+        <!-- Tabella per mostrare la lista degli hotel -->
         <table class="table">
             <thead>
                 <tr>
-                    <th scope="col">Name</th>
-                    <th scope="col">Description</th>
-                    <th scope="col">Parking</th>
-                    <th scope="col">Vote</th>
-                    <th scope="col">Distance to center</th>
+                    <th scope="col">Nome</th>
+                    <th scope="col">Descrizione</th>
+                    <th scope="col">Parcheggio</th>
+                    <th scope="col">Voto</th>
+                    <th scope="col">Distanza dal centro</th>
                 </tr>
             </thead>
-            <!-- foreach che comprende la sezione su cui voglio iterare in modo da escludere i vari th sopra -->
-            <?php foreach ($hotels as $hotel) { ?>
-                <tbody>
-                    <tr>
-                        <th scope="row"><?php echo $hotel['name'] . '<br>'; ?></th>
-                        <td><?php echo $hotel['description'] . '<br>'; ?></td>
-                        <td><?php echo ($hotel['parking'] ? 'Parking available' : 'No parking') . '<br>'; ?></td>
-                        <td><?php echo 'Vote: ' . $hotel['vote'] . '<br>'; ?></td>
-                        <td><?php echo 'Distance to center: ' . $hotel['distance_to_center'] . ' km <br>'; ?></td>
-                    </tr>
-                <?php } ?>
-                </tbody>
+            <tbody>
+                <?php
+                // Ciclo attraverso ciascun hotel nell'array degli hotels
+                foreach ($hotels as $hotel) {
+                    // Filtro hotel in base al parcheggio se il checkbox è selezionato
+                    if (isset($_GET['parking']) && !$hotel['parking']) {
+                        continue; 
+                    }
+                    echo "<tr>
+                        <th scope=\"row\">{$hotel['name']}</th>
+                        <td>{$hotel['description']}</td>
+                        <td>" . ($hotel['parking'] ? 'Parcheggio disponibile' : 'Nessun parcheggio') . "</td>
+                        <td>Voto: {$hotel['vote']}</td>
+                        <td>Distanza dal centro: {$hotel['distance_to_center']} km</td>
+                    </tr>";
+                }
+                ?>
+            </tbody>
         </table>
     </main>
 </body>
 
 </html>
-
